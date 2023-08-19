8e16be22e4fa909b5cb027865ab9a6cf63cba823
Zomcules <hisabless@gmail.com>
2023-08-19 08:42:35 +0200

    after firebase :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:samsarah/tab/home_page.dart';
 import 'package:samsarah/util/database/database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   await DataBase().init();
 
