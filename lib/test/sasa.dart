91627dc9524456ad0c251cede1a80600b1e60553
Zomcules <hisabless@gmail.com>
2023-08-19 08:43:15 +0200

    after firebase again :(

--- lib/test/sasa.dart
+++ lib/test/sasa.dart
@@ -1,6 +1,21 @@
 // ignore_for_file: avoid_print
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 void main(List<String> args) {
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+  users
+      .add({
+        'full_name': 'mohamed', // John Doe
+        'company': 'for the streets', // Stokes and Sons
+        'age': 50 // 42
+      })
+      .then((value) => print("User Added"))
+      .catchError((error) => print("Failed to add user: $error"));
+
   List<Person> list = [
     Person(name: "ahmed", wife: Wife(name: "Amna")),
     Person(name: "ali", wife: Wife(name: "Mona"))
