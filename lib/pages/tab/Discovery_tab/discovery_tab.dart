ff2d76c13eb6fa98ed4f5470d911cc24b90de3e8
Zomcules <hisabless@gmail.com>
2023-09-21 18:27:12 +0200

    last stream to be fixed

--- lib/pages/tab/Discovery_tab/discovery_tab.dart
+++ lib/pages/tab/Discovery_tab/discovery_tab.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:samsarah/services/firestore_service.dart';
 import 'package:samsarah/pages/tab/Discovery_tab/discovery_header.dart';
 import 'package:samsarah/pages/tab/Discovery_tab/product_snackbar.dart';
+
+import '../../../modules/product_info.dart';
 
 class DiscoveryTab extends StatefulWidget {
   const DiscoveryTab({super.key});
@@ -12,7 +15,12 @@ class DiscoveryTab extends StatefulWidget {
 
 class _DiscoveryTabState extends State<DiscoveryTab>
     with AutomaticKeepAliveClientMixin {
-  var productSnackbars = getDummyProductSnackbars();
+  late Stream<QuerySnapshot<ProductInfo>> _stream;
+  @override
+  void initState() {
+    super.initState();
+    _stream = FireStoreService().productCollection.snapshots();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +31,7 @@ class _DiscoveryTabState extends State<DiscoveryTab>
         const DisHeader(),
         Expanded(
           child: StreamBuilder(
-              stream: FireStoreService().productCollection.snapshots(),
+              stream: _stream,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
                   return ListView.builder(
